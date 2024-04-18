f5051b6af06b01d544e7a50efebf8e81acbc46a0
bslbckr <basil@ultical.com>
2024-04-19 01:10:57 +0200

    Delete unused validationappliesTo attribute

--- members-backend/src/main/java/de/guc/dto/ValidOnBoarding.java
+++ members-backend/src/main/java/de/guc/dto/ValidOnBoarding.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import jakarta.validation.ConstraintTarget;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
@@ -13,7 +12,6 @@ import jakarta.validation.Payload;
 @Target({ ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidOnBoarding {
-    //ConstraintTarget validationAppliesTo() default ConstraintTarget.IMPLICIT;
     String message() default "If the new member is a 'child' the fields memberFirstName and memberName must not be empty";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
