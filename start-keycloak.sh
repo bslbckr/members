ae91e832dc458454b9bd2171c03be5d206d64836
bslbckr <basil@ultical.com>
2024-10-15 02:25:05 +0200

    Update to keycloak 26.0

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -6,5 +6,5 @@ podman run \
        --env KEYCLOAK_ADMIN=guc \
        --env KEYCLOAK_ADMIN_PASSWORD=potsdam \
        -v $(pwd)/members-backend/src/test/resources/keycloak:/opt/keycloak/data/import:Z \
-       quay.io/keycloak/keycloak:23.0.1 \
+       quay.io/keycloak/keycloak:26.0 \
        start-dev --import-realm 
