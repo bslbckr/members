f2ea64ec31e59c1b3ab97254577ac6e8ef70eee8
bslbckr <basil@ultical.com>
2024-12-13 01:02:02 +0100

    Use new hasRole-guards

--- web-ui/src/app/app-routing.module.ts
+++ web-ui/src/app/app-routing.module.ts
@@ -3,22 +3,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { authenticatedGuard } from './authenticated.guard';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { hasBoardMemberRole, hasOnBoardingRoleGuard } from './hasrole.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    {
-        path: 'start',
-        loadChildren: () => import('./start/start.module').then(m => m.StartModule),
-        canActivateChild: [authenticatedGuard]
-    },
-    {
-        path: 'member',
-        loadChildren: () => import('./member/member.module').then(m => m.MemberModule),
-        canActivateChild: [authenticatedGuard]
-    },
-    { path: 'onboarding', loadChildren: () => import('./on.boarding/on.boarding.module').then(m => m.OnBoardingModule) },
-    { path: 'changes', loadChildren: () => import('./changes/changes.module').then(m => m.ChangesModule) },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'start',
+    loadChildren: () => import('./start/start.module').then(m => m.StartModule),
+    canActivateChild: [authenticatedGuard]
+  },
+  {
+    path: 'member',
+    loadChildren: () => import('./member/member.module').then(m => m.MemberModule),
+    canActivateChild: [authenticatedGuard]
+  },
+  {
+    path: 'onboarding',
+    loadChildren: () => import('./on.boarding/on.boarding.module').then(m => m.OnBoardingModule),
+    canActivateChild: [authenticatedGuard, hasOnBoardingRoleGuard]
+  },
+  {
+    path: 'changes',
+    loadChildren: () => import('./changes/changes.module').then(m => m.ChangesModule),
+    canActivateChild: [authenticatedGuard, hasBoardMemberRole]
+  },
     {
         path: '**',
         component: AppComponent
