7ea68b4110e5b527e679eb38080aaa21462418a0
bslbckr <basil@ultical.com>
2025-02-17 23:35:18 +0100

    Generate component and service stubs

---
+++ web-ui/src/app/member-overview/member-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-overview.component.html',
+  styleUrl: './member-overview.component.css'
+})
+export class MemberOverviewComponent {
+
+}
