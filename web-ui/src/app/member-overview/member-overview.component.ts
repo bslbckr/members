e4e0b191e878968f68d087024711e54429bc2182
bslbckr <basil@ultical.com>
2025-02-20 01:10:17 +0100

    Add table, paginator and service to member-overview

--- web-ui/src/app/member-overview/member-overview.component.ts
+++ web-ui/src/app/member-overview/member-overview.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MemberOverview, MemberOverviewService } from './member-overview.service';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-member-overview',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './member-overview.component.html',
   styleUrl: './member-overview.component.css'
 })
-export class MemberOverviewComponent {
+export class MemberOverviewComponent implements OnInit, AfterViewInit{
+  private readonly service = inject(MemberOverviewService);
+  readonly displayedColumns = ["givenName", "name", "entryDate", "state", "stateEffective", "exitDate"];
 
+  readonly datasource = new MatTableDataSource<MemberOverview>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+  ngOnInit() {
+    this.service.loadMemberOverview().subscribe(data => {
+      this.datasource.data = data;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.datasource.paginator = this.paginator;
+  }
 }
--- web-ui/src/app/member-overview/member-overview.service.ts
+++ web-ui/src/app/member-overview/member-overview.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, take } from 'rxjs';
+
+export interface MemberOverview {
+  givenName: string;
+  name: string;
+  state: string;
+  stateEffective: Date;
+   entryDate: Date;
+  exitDate: Date;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberOverviewService {
+  private readonly overviewUrl:string = "/api/v1/member";
+  private readonly http = inject(HttpClient);
 
-  constructor() { }
+  loadMemberOverview():Observable<MemberOverview[]> {
+    return this.http.get<MemberOverview[]>(this.overviewUrl).pipe(take(1));
+  }
 }
