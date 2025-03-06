3f915088ec2ae38667619f59e7c049207f45f130
bslbckr <basil@ultical.com>
2025-03-06 01:23:02 +0100

    Add sorting capabilities to overview table

--- web-ui/src/app/member-overview/member-overview.component.ts
+++ web-ui/src/app/member-overview/member-overview.component.ts
@@ -3,10 +3,11 @@ import { MemberOverview, MemberOverviewService } from './member-overview.service
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { DatePipe } from '@angular/common';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 
 @Component({
     selector: 'app-member-overview',
-    imports: [MatTableModule, MatPaginatorModule, DatePipe],
+  imports: [MatTableModule, MatPaginatorModule, DatePipe, MatSortModule],
     templateUrl: './member-overview.component.html',
     styleUrl: './member-overview.component.css',
     providers: [MemberOverviewService]
@@ -17,14 +18,19 @@ export class MemberOverviewComponent implements OnInit, AfterViewInit{
 
   readonly datasource = new MatTableDataSource<MemberOverview>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   
+  private rawData!: MemberOverview[];
   ngOnInit() {
     this.service.loadMemberOverview().subscribe(data => {
       this.datasource.data = data;
+      this.rawData = data;
     });
   }
 
   ngAfterViewInit() {
     this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
   }
+
 }
