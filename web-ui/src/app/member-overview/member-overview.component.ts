d6a33ab4c9c451d05c68de99519cad535f562fe0
bslbckr <basil@ultical.com>
2025-03-11 23:52:55 +0100

    Add simple filter to table

--- web-ui/src/app/member-overview/member-overview.component.ts
+++ web-ui/src/app/member-overview/member-overview.component.ts
@@ -1,13 +1,20 @@
-import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { MemberOverview, MemberOverviewService } from './member-overview.service';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { DatePipe } from '@angular/common';
-import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { debounceTime, distinctUntilChanged, fromEvent, tap } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatFormField, MatInput, MatLabel, MatSuffix } from '@angular/material/input';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
 
 @Component({
     selector: 'app-member-overview',
-  imports: [MatTableModule, MatPaginatorModule, DatePipe, MatSortModule],
+  imports: [MatTableModule, MatPaginatorModule, DatePipe, MatSortModule, MatCardModule, MatToolbar, MatInput, MatFormField, MatLabel, MatSuffix, MatIconButton, MatIcon],
     templateUrl: './member-overview.component.html',
     styleUrl: './member-overview.component.css',
     providers: [MemberOverviewService]
@@ -19,18 +26,35 @@ export class MemberOverviewComponent implements OnInit, AfterViewInit{
   readonly datasource = new MatTableDataSource<MemberOverview>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild("filterInput") filterInput!: ElementRef;
   
-  private rawData!: MemberOverview[];
   ngOnInit() {
     this.service.loadMemberOverview().subscribe(data => {
       this.datasource.data = data;
-      this.rawData = data;
     });
   }
 
   ngAfterViewInit() {
     this.datasource.paginator = this.paginator;
     this.datasource.sort = this.sort;
+
+    fromEvent(this.filterInput.nativeElement, "input", {once: false})
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap(async (e) => {
+          const filter = (this.filterInput.nativeElement as HTMLInputElement).value;
+          this.datasource.filter = filter;
+        }))
+      .subscribe();
+  }
+
+  filterReset(): void {
+    this.filterInput.nativeElement.value = "";
+    this.filterInput.nativeElement.dispatchEvent(new Event("input", {
+      bubbles: true,
+      cancelable: true
+    }));
   }
 
 }
