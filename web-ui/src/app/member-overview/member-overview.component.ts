cec6e79d2e1375e87ff6e3a8a8b5e8facb42db56
Basil <15678130+bslbckr@users.noreply.github.com>
2025-03-19 00:31:00 +0100

    Sort member overview (#58)
    * Add sorting capabilities to overview table
    
    * Add simple filter to table
    
    * Add CSV Export functionality

--- web-ui/src/app/member-overview/member-overview.component.ts
+++ web-ui/src/app/member-overview/member-overview.component.ts
@@ -1,22 +1,34 @@
-import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { MemberOverview, MemberOverviewService } from './member-overview.service';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { DatePipe } from '@angular/common';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { debounceTime, distinctUntilChanged, fromEvent, tap } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatFormField, MatInput, MatLabel, MatSuffix } from '@angular/material/input';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { GenericCsvService } from './generic-csv.service';
+
 
 @Component({
     selector: 'app-member-overview',
-    imports: [MatTableModule, MatPaginatorModule, DatePipe],
+  imports: [MatTableModule, MatPaginatorModule, DatePipe, MatSortModule, MatCardModule, MatToolbar, MatInput, MatFormField, MatLabel, MatSuffix, MatIconButton, MatIcon],
     templateUrl: './member-overview.component.html',
     styleUrl: './member-overview.component.css',
-    providers: [MemberOverviewService]
+  providers: [MemberOverviewService, GenericCsvService]
 })
 export class MemberOverviewComponent implements OnInit, AfterViewInit{
   private readonly service = inject(MemberOverviewService);
+  private readonly csv = inject(GenericCsvService);
   readonly displayedColumns = ["givenName", "name", "entryDate", "state", "stateEffective", "exitDate"];
 
   readonly datasource = new MatTableDataSource<MemberOverview>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild("filterInput") filterInput!: ElementRef;
   
   ngOnInit() {
     this.service.loadMemberOverview().subscribe(data => {
@@ -26,5 +38,42 @@ export class MemberOverviewComponent implements OnInit, AfterViewInit{
 
   ngAfterViewInit() {
     this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+
+    fromEvent(this.filterInput.nativeElement, "input", {once: false})
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap(async (e) => {
+          const filter = (this.filterInput.nativeElement as HTMLInputElement).value;
+          this.datasource.filter = filter;
+        }))
+      .subscribe();
+  }
+
+  filterReset(): void {
+    this.filterInput.nativeElement.value = "";
+    this.filterInput.nativeElement.dispatchEvent(new Event("input", {
+      bubbles: true,
+      cancelable: true
+    }));
+  }
+
+  downloadCsv() {
+    const csvHeaders:{[key in keyof MemberOverview]:string}  = {
+      "givenName": "Vorname",
+      "name": "Nachname",
+      "entryDate": "Eintrittsdatum",
+      "exitDate": "Austrittsdatum",
+      "state": "Status",
+      "stateEffective": "Status seit"
+    };
+    const exportedCsv = this.csv.toCsv(this.datasource.filteredData, { header: csvHeaders, delimiter: ";"});
+    const blob = URL.createObjectURL(new Blob([exportedCsv]))
+    const anchor = document.createElement("a");
+    anchor.href = blob;
+    anchor.download = "GUC-Mitglieder.csv";
+    anchor.click();
+    URL.revokeObjectURL(blob);
   }
 }
