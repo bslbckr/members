777087dd24fb50b15b0368a5eaac7e5ffa5052d2
bslbckr <basil@ultical.com>
2025-10-24 00:51:09 +0200

    fix: make home component standalone

--- web-ui/src/app/home/home.component.ts
+++ web-ui/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    standalone: false
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  imports: [RouterModule]
 })
 export class HomeComponent {
 
