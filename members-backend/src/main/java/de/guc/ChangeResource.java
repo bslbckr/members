ec6f408dcf383e269c9ec002af216c1fb273a4ae
bslbckr <basil@ultical.com>
2025-11-19 02:33:01 +0100

    feature: include list of entries and cancellations in changeresource

--- members-backend/src/main/java/de/guc/ChangeResource.java
+++ members-backend/src/main/java/de/guc/ChangeResource.java
@@ -1,9 +1,12 @@
 package de.guc;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Objects;
 
 import de.guc.dto.StateChangeDto;
+import de.guc.entities.EntryOrCancellation;
+import de.guc.entities.MemberEntity;
 import de.guc.services.HistoryService;
 import io.quarkus.security.Authenticated;
 import jakarta.annotation.security.RolesAllowed;
@@ -26,10 +29,26 @@ public class ChangeResource {
     @GET
     @Path("state")
     @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed("board-member")
+    @RolesAllowed(Constants.ROLE_BOARD_MEMBER)
     @Transactional
     public Collection<StateChangeDto> getStateChanges() {
         final var changes = this.service.stateChanges();
         return changes.stream().map(StateChangeDto::fromStateChange).toList();
+    }
+
+    @GET
+    @Path("cancellation")
+    @RolesAllowed(Constants.ROLE_BOARD_MEMBER)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<EntryOrCancellation> getCancellations() {
+        return this.service.cancellations(LocalDate.now().minusMonths(18));
+    }
+
+    @GET
+    @Path("entries")
+    @RolesAllowed(Constants.ROLE_BOARD_MEMBER)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<EntryOrCancellation> getEntries() {
+        return MemberEntity.entriesSinceDate(LocalDate.now().minusMonths(18));
     }
 }
