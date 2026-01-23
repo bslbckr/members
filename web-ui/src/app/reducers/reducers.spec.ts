089513f084d6b52ac516e742164b11f4ac3dd4ff
Basil <15678130+bslbckr@users.noreply.github.com>
2026-01-23 03:56:27 +0100

    Chore/update karma (#234)
    * Migrate tests from karma to vitest

--- web-ui/src/app/reducers/reducers.spec.ts
+++ web-ui/src/app/reducers/reducers.spec.ts
@@ -1,22 +1,21 @@
 import { onBoardingActions } from '../actions/actions';
-import {initialState, OnBoardingReducer} from './reducers';
+import { initialState, OnBoardingReducer } from './reducers';
 
 describe('OnBoarding Reducer', () => {
 
-  it('should set success', () => {
-    const state = OnBoardingReducer(initialState, onBoardingActions.success());
-    expect(state.onBoardingSuccessful).toBeTrue();
-  });
+    it('should set success', () => {
+        const state = OnBoardingReducer(initialState, onBoardingActions.success());
+        expect(state.onBoardingSuccessful).toBe(true);
+    });
 
-  it('should reset success state', () => {
-    const state = OnBoardingReducer({onBoardingSuccessful: false, errorMessage: "foo"},
-      onBoardingActions.start({firstname: "foo", name: "bar", email: "test", isChild: false, memberFirstName: "", memberName: "", login: "login"}));
-    expect(state.onBoardingSuccessful).toBeNull();
-  });
+    it('should reset success state', () => {
+        const state = OnBoardingReducer({ onBoardingSuccessful: false, errorMessage: "foo" }, onBoardingActions.start({ firstname: "foo", name: "bar", email: "test", isChild: false, memberFirstName: "", memberName: "", login: "login" }));
+        expect(state.onBoardingSuccessful).toBeNull();
+    });
 
-  it('should set failure', () => {
-    const state = OnBoardingReducer(initialState, onBoardingActions.failure({message: "test failed"}));
-    expect(state.onBoardingSuccessful).toBeFalse();
-    expect(state.errorMessage).toBe("test failed");
-  });
-})
+    it('should set failure', () => {
+        const state = OnBoardingReducer(initialState, onBoardingActions.failure({ message: "test failed" }));
+        expect(state.onBoardingSuccessful).toBe(false);
+        expect(state.errorMessage).toBe("test failed");
+    });
+});
